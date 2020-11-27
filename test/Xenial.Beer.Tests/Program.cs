b9b9a02d308f3ef2f032eac8898ca56d45c35cee
Manuel Grundner <m.grundner@delegate.at>
2020-11-27 12:09:30 +0100

    feat: initial commit

---
+++ test/Xenial.Beer.Tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+using static Xenial.Tasty;
+
+namespace Xenial.Delicious.Beer.Tests
+{
+    public static class Program
+    {
+        public static async Task<int> Main(string[] args)
+            => await Run(args);
+    }
+}
+
