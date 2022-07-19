d0cf55c29145108e7039c43308feae87c122faf1
Manuel Grundner <m.grundner@delegate.at>
2022-07-19 13:19:48 +0200

    feat: new namespace references

--- build/Program.Helpers.cs
+++ build/Program.Helpers.cs
@@ -5,30 +5,29 @@ using System.Xml.Linq;
 
 using static SimpleExec.Command;
 
-namespace Xenial.Build
-{
-    internal static partial class Program
-    {
-        public static async Task EnsureTools()
-        {
-            try
-            {
-                await RunAsync("dotnet", "format --version");
-            }
-            catch (SimpleExec.ExitCodeException)
-            {
-                //Can't find dotnet format, assuming tools are not installed
-                await RunAsync("dotnet", "tool restore");
-            }
-        }
+namespace Xenial.Build;
 
-        public static string Tabify(string s)
+internal static partial class Program
+{
+    public static async Task EnsureTools()
+    {
+        try
         {
-            s = s ?? string.Empty;
-            return string.Join(
-                           Environment.NewLine,
-                           s.Split("\n").Select(s => $"\t{s}")
-                       );
+            await RunAsync("dotnet", "format --version");
         }
+        catch (SimpleExec.ExitCodeException)
+        {
+            //Can't find dotnet format, assuming tools are not installed
+            await RunAsync("dotnet", "tool restore");
+        }
+    }
+
+    public static string Tabify(string s)
+    {
+        s = s ?? string.Empty;
+        return string.Join(
+                       Environment.NewLine,
+                       s.Split("\n").Select(s => $"\t{s}")
+                   );
     }
 }
--- build/Program.cs
+++ build/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Runtime.InteropServices;
 
 using static SimpleExec.Command;
 using static Bullseye.Targets;
@@ -8,93 +7,92 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.IO;
 
-namespace Xenial.Build
+namespace Xenial.Build;
+
+internal static partial class Program
 {
-    internal static partial class Program
+    internal static async Task Main(string[] args)
     {
-        internal static async Task Main(string[] args)
+        static string logOptions(string target)
+            => $"/maxcpucount /nologo /verbosity:minimal /bl:./artifacts/logs/Beer.{target}.binlog";
+
+        const string Configuration = "Release";
+
+        Func<string> properties = () => string.Join(" ", new Dictionary<string, string>
         {
-            static string logOptions(string target)
-                => $"/maxcpucount /nologo /verbosity:minimal /bl:./artifacts/logs/Beer.{target}.binlog";
+            ["Configuration"] = Configuration,
+        }.Select(p => $"/P:{p.Key}=\"{p.Value}\""));
 
-            const string Configuration = "Release";
+        Target("ensure-tools", () => EnsureTools());
 
-            Func<string> properties = () => string.Join(" ", new Dictionary<string, string>
+        Target("clean", DependsOn("ensure-tools"),
+            () => RunAsync("dotnet", $"rimraf . -i **/bin/**/*.* -i **/obj/**/*.* -i artifacts/**/*.* -e node_modules/**/*.* -e build/**/*.* -q")
+        );
+
+        Target("restore",
+            () => RunAsync("dotnet", $"restore {logOptions("restore")}")
+        );
+
+        Target("build", DependsOn("restore"),
+            () => RunAsync("dotnet", $"build --no-restore -c {Configuration} {logOptions("build")} {properties()}")
+        );
+
+        Target("test", DependsOn("build"), async () =>
+        {
+            var tfms = new[] { "net5", "net6.0" };
+
+            var tests = tfms
+                .Select(tfm => RunAsync("dotnet", $"run --project test/Xenial.Beer.Tests/Xenial.Beer.Tests.csproj --no-build --no-restore --framework {tfm} -c {Configuration} {properties()}"))
+                .ToArray();
+
+            await Task.WhenAll(tests);
+        });
+
+        Target("lic", DependsOn("test"),
+            async () =>
             {
-                ["Configuration"] = Configuration,
-            }.Select(p => $"/P:{p.Key}=\"{p.Value}\""));
-
-            Target("ensure-tools", () => EnsureTools());
-
-            Target("clean", DependsOn("ensure-tools"),
-                () => RunAsync("dotnet", $"rimraf . -i **/bin/**/*.* -i **/obj/**/*.* -i artifacts/**/*.* -e node_modules/**/*.* -e build/**/*.* -q")
-            );
-
-            Target("restore",
-                () => RunAsync("dotnet", $"restore {logOptions("restore")}")
-            );
-
-            Target("build", DependsOn("restore"),
-                () => RunAsync("dotnet", $"build --no-restore -c {Configuration} {logOptions("build")} {properties()}")
-            );
-
-            Target("test", DependsOn("build"), async () =>
-            {
-                var tfms = new[] { "net5", "net6.0" };
-
-                var tests = tfms
-                    .Select(tfm => RunAsync("dotnet", $"run --project test/Xenial.Beer.Tests/Xenial.Beer.Tests.csproj --no-build --no-restore --framework {tfm} -c {Configuration} {properties()}"))
-                    .ToArray();
-
-                await Task.WhenAll(tests);
-            });
-
-            Target("lic", DependsOn("test"),
-                async () =>
+                var files = Directory.EnumerateFiles(@"src", "*.csproj", SearchOption.AllDirectories).Select(file => new
                 {
-                    var files = Directory.EnumerateFiles(@"src", "*.csproj", SearchOption.AllDirectories).Select(file => new
-                    {
-                        ProjectName = $"src/{Path.GetFileNameWithoutExtension(file)}/{Path.GetFileName(file)}",
-                        ThirdPartyName = $"src/{Path.GetFileNameWithoutExtension(file)}/THIRD-PARTY-NOTICES.TXT"
-                    });
+                    ProjectName = $"src/{Path.GetFileNameWithoutExtension(file)}/{Path.GetFileName(file)}",
+                    ThirdPartyName = $"src/{Path.GetFileNameWithoutExtension(file)}/THIRD-PARTY-NOTICES.TXT"
+                });
 
-                    var tasks = files.Select(proj => RunAsync("dotnet", $"thirdlicense --project {proj.ProjectName} --output {proj.ThirdPartyName}"));
+                var tasks = files.Select(proj => RunAsync("dotnet", $"thirdlicense --project {proj.ProjectName} --output {proj.ThirdPartyName}"));
 
-                    await Task.WhenAll(tasks);
-                }
-            );
+                await Task.WhenAll(tasks);
+            }
+        );
 
-            Target("pack", DependsOn("lic"),
-                () => RunAsync("dotnet", $"pack Xenial.Beer.sln --no-restore --no-build -c {Configuration} {logOptions("pack.nuget")} {properties()}")
-            );
+        Target("pack", DependsOn("lic"),
+            () => RunAsync("dotnet", $"pack Xenial.Beer.sln --no-restore --no-build -c {Configuration} {logOptions("pack.nuget")} {properties()}")
+        );
 
-            Target("docs",
-                () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs")
-            );
+        Target("docs",
+            () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs")
+        );
 
-            Target("docs.serve", DependsOn("ensure-tools"),
-                () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs -w -p")
-            );
+        Target("docs.serve", DependsOn("ensure-tools"),
+            () => RunAsync("dotnet", "wyam docs -o ../artifacts/docs -w -p")
+        );
 
-            Target("deploy.nuget", DependsOn("ensure-tools"), async () =>
+        Target("deploy.nuget", DependsOn("ensure-tools"), async () =>
+        {
+            var files = Directory.EnumerateFiles("artifacts/nuget", "*.nupkg");
+
+            foreach (var file in files)
             {
-                var files = Directory.EnumerateFiles("artifacts/nuget", "*.nupkg");
+                await RunAsync("dotnet", $"nuget push {file} --skip-duplicate -s https://api.nuget.org/v3/index.json -k {Environment.GetEnvironmentVariable("NUGET_AUTH_TOKEN")}",
+                    noEcho: true
+                );
+            }
+        });
 
-                foreach (var file in files)
-                {
-                    await RunAsync("dotnet", $"nuget push {file} --skip-duplicate -s https://api.nuget.org/v3/index.json -k {Environment.GetEnvironmentVariable("NUGET_AUTH_TOKEN")}",
-                        noEcho: true
-                    );
-                }
-            });
+        Target("release",
+            () => Release()
+        );
 
-            Target("release",
-                () => Release()
-            );
+        Target("default", DependsOn("test"));
 
-            Target("default", DependsOn("test"));
-
-            await RunTargetsAndExitAsync(args);
-        }
+        await RunTargetsAndExitAsync(args);
     }
 }
